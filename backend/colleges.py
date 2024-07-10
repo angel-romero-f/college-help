28f251243ec0e2228db17152900cbafc43de652b
Edder Hernandez Gomez <ehernandezgomez@vassar.edu>
2024-07-10 18:34:15 +0000

    started main.py, started creating an html table in main.py, added templates, and requirements.txt. Currently the flask app wont run because of the colleges.py. the data from the api needs to be filtered, so if you need to run the flask app just comment out the import colleges.py statement from main.py, and commend out the code for the college info path and the row/column variables

--- backend/colleges.py
+++ backend/colleges.py
@@ -1,1 +1,49 @@
-# this file is for fetching data from the api 
+# this file is for fetching data from the api import requests
+import requests
+import json
+import sqlalchemy as db
+import pandas as pd
+import pprint
+
+# Base URL
+URL = "https://api.data.gov/ed/collegescorecard/v1/schools"
+
+# function that makes a call to the api and passess in neccesary parameters
+def get_college_info(WE_CAN_PASS_IN_PARAMS_HERE):
+    global URL
+
+    # I used a placeholder parameter for now, but we append the ones that are passed in as input into the "params" dictionary
+    params = {
+    'api_key':"9cx6Llu0TXhNipn0XjML7TTictmFY8eKAJtmn2aQ",
+    'school.name':'Harvard University',
+    }
+
+    return requests.get(URL, params=params)
+
+#print(json.dumps(get_college_info("f").json(), indent=3))
+
+######################
+## FILTER DATA HERE 
+##
+##
+######################
+
+#convert resulting dictionary to pandas dataframe
+# this df will be used in the main.py file
+# the "f" is a placeholder for now
+ret = dict(get_college_info("f").json())
+college_info_db = pd.DataFrame.from_dict(ret)
+
+
+
+
+
+
+#### note: I found everything below this point to be extra, so I did not include it ####
+
+## convert dataframe to sql, and create a file with with the data with a .db extension ##
+#engine = db.create_engine('sqlite:///college_info.db')
+#college_info_db.to_sql('college', con=engine, if_exists='replace', index=False)
+
+#with engine.connect() as connection:
+#   query_result = connection.execute(db.text("SELECT * FROM college;")).fetchall()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
+from colleges.py import college_info_db
 
 app = Flask(__name__)
+
+# Create variables that hold columns and rows from the table
+cols = [col for col in college_info_db]
+rows = []
+for index, row in college_info_db.iterrows():
+    rows.append(row)
+    # note, you can access a value from a row using row["column"]
 
 app.route('/')
 def home():
     return 'Hello, World!'
+
+app.route('/display_college_info')
+def display():
+    return render_template('display_college_info.html', cols=cols, rows=rows)
+
+
 
 if __name__ == '__main__':
     app.run()
