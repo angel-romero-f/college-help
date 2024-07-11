d5f469dfaf767a3166f6f06b05e5a6f1fdcb6e1e
Edder Hernandez Gomez <ehernandezgomez@vassar.edu>
2024-07-11 21:28:35 +0000

    fixed small typos

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from colleges.py import college_info_db
+from colleges import college_info_db
 
 app = Flask(__name__)
 
@@ -10,15 +10,15 @@ for index, row in college_info_db.iterrows():
     rows.append(row)
     # note, you can access a value from a row using row["column"]
 
-app.route('/')
+@app.route('/')
 def home():
     return 'Hello, World!'
 
-app.route('/display_college_info')
+@app.route('/display_college_info')
 def display():
     return render_template('display_college_info.html', cols=cols, rows=rows)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
