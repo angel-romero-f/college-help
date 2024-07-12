1857252d9df8850f3f8dd2842dbff1380cc013d7
angel-romero-f <angelfloromero@gmail.com>
2024-07-12 08:53:28 -0500

    Change max length

--- backend/forms.py
+++ backend/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    nameSearch = StringField('nameSearch', validators=[DataRequired(), Length(min=2, max=20)])
+    nameSearch = StringField('nameSearch', validators=[DataRequired(), Length(min=2, max=50)])
     submit = SubmitField('submit')
