dd213c66023dfc0fed646aebb130ad10d137d0fe
angel-romero-f <angelfloromero@gmail.com>
2024-07-11 22:28:51 -0500

    Connect backend and frontend (sort of)

--- backend/colleges.py
+++ backend/colleges.py
@@ -9,65 +9,40 @@ import pprint
 URL = "https://api.data.gov/ed/collegescorecard/v1/schools"
 
 # function that makes a call to the api and passess in neccesary parameters
-def get_college_info(WE_CAN_PASS_IN_PARAMS_HERE):
+def get_college_info(college_name):
     global URL
 
     # I used a placeholder parameter for now, but we append the ones that are passed in as input into the "params" dictionary
     params = {
     'api_key':"9cx6Llu0TXhNipn0XjML7TTictmFY8eKAJtmn2aQ",
-    'school.name':'Harvard University',
+    'school.name': college_name,
     }
 
-    return requests.get(URL, params=params)
+    ret = requests.get(URL, params=params)
+    def save_to_json(data, filename):
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
 
-#print(json.dumps(get_college_info("f").json(), indent=3))
+    ret = get_college_info("f").json()
 
-######################
-## FILTER DATA HERE 
-##
-##
-######################
+    save_to_json(ret, 'college_info.json')
+            
+    df = pd.json_normalize(ret['results'])
 
-#convert resulting dictionary to pandas dataframe
-# this df will be used in the main.py file
-# the "f" is a placeholder for now
+    df = df[['latest.school.name', 'latest.school.city', 'latest.school.state', 'latest.student.size', 'latest.cost.tuition.in_state']]
 
+    engine = db.create_engine('sqlite:///locations.db')
 
-def save_to_json(data, filename):
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    df.to_sql('colleges', con=engine, if_exists='append', index=False)
 
-ret = get_college_info("f").json()
+    df.rename(columns={'latest.school.name':'Name', 'latest.school.city':'City', 'latest.school.state':'State', 'latest.student.size':'Size', 'latest.cost.tuition.in_state':'Tuition'}, inplace=True)
 
-save_to_json(ret, 'college_info.json')
+    query_result = ""
+    with engine.connect() as connection:
+        query_result = connection.execute(db.text("SELECT * FROM colleges WHERE Name = college_name;")).fetchall()
+        print(pd.DataFrame(query_result))
         
-df = pd.json_normalize(ret['results'])
-
-df = df[['latest.school.name', 'latest.school.city', 'latest.school.state', 'latest.student.size', 'latest.cost.tuition.in_state']]
-
-engine = db.create_engine('sqlite:///locations.db')
-
-df.to_sql('colleges', con=engine, if_exists='append', index=False)
-
-df.rename(columns={'latest.school.name':'Name', 'latest.school.city':'City', 'latest.school.state':'State', 'latest.student.size':'Size', 'latest.cost.tuition.in_state':'Tuition'}, inplace=True)
-
-with engine.connect() as connection:
-    query_result = connection.execute(db.text("SELECT * FROM colleges;")).fetchall()
-    print(pd.DataFrame(query_result))
+    result = [dict(row) for row in query_result]
+    return result
 
 
-
-
-
-
-
-
-
-#### note: I found everything below this point to be extra, so I did not include it ####
-
-## convert dataframe to sql, and create a file with with the data with a .db extension ##
-#engine = db.create_engine('sqlite:///college_info.db')
-#college_info_db.to_sql('college', con=engine, if_exists='replace', index=False)
-
-#with engine.connect() as connection:
-#   query_result = connection.execute(db.text("SELECT * FROM college;")).fetchall()
--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, url_for
-from colleges import college_info_db
+from flask import Flask, render_template, request, jsonify
+from colleges import get_college_info
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Create variables that hold columns and rows from the table
-cols = [col for col in college_info_db]
-rows = []
-for index, row in college_info_db.iterrows():
-    rows.append(row)
-    # note, you can access a value from a row using row["column"]
+
+# # Create variables that hold columns and rows from the table
+# cols = [col for col in college_info_db]
+# rows = []
+# for index, row in college_info_db.iterrows():
+#     rows.append(row)
+#     # note, you can access a value from a row using row["column"]
 
 @app.route('/')
 def home():
@@ -18,7 +21,13 @@ def home():
 def display():
     return render_template('display_college_info.html', cols=cols, rows=rows)
 
+@app.route('/search_college', methods=['GET','POST'])
+def search_college():
+    data = request.get_json()
+    college_name = data.get('collegeName')
+    result = get_college_info(college_name)
 
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
